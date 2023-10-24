bc047d162c6442d4c06d7c587d9b69ed5e020bc8
shafira <shafiravandeyli@gmail.com>
2023-10-25 00:58:07 +0700

    navbar

--- detailMapelSiswa.js
+++ detailMapelSiswa.js
@@ -1,12 +1,12 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 let item = document.querySelector(".beranda a");
 var i;
+let dropDownMapel = document.querySelector(".dropdown-container");
 
+
+dropDownMapel.style.display = "block";
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
-    item.style.color = "#32415d";
-    item.style.background = "none";
-    this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
@@ -17,23 +17,18 @@ for (i = 0; i < dropdown.length; i++) {
 }
 
 
-let dropDownMapel = document.querySelector(".dropdown-container");
-// console.log(dropDownMapel)
 let getMapel = async () => {
   let response = await fetch(
     `https://63819b489842ca8d3c9642d0.mockapi.io/mapel`
   );
   let mapel = await response.json();
   let print = "";
-  let print1 = "";
   mapel.forEach((item) => {
     print += `<a class="btn" href="detailMapelSiswa.html">${item.name}</a>`;
-    // detailMapelSiswa.html
-  
+
     dropDownMapel.innerHTML = print;
     console.log(`${item.name}`);
     localStorage.setItem("username", print.name);
-    // localStorage.setItem("pw", password.value);
   });
 };
 
--- siswa.js
+++ siswa.js
@@ -1,12 +1,13 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 let item = document.querySelector(".beranda a");
 var i;
-
+item.style.color = "#d13737";
+item.style.backgroundColor = "#d9d9d9";
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
     item.style.color = "#32415d";
     item.style.background = "none";
-    this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
@@ -18,7 +19,6 @@ for (i = 0; i < dropdown.length; i++) {
 
 let dropDownMapel = document.querySelector(".dropdown-container");
 let cardMapel = document.querySelector("#mapel .row");
-// console.log(dropDownMapel)
 let getMapel = async () => {
   let response = await fetch(
     `https://63819b489842ca8d3c9642d0.mockapi.io/mapel`
@@ -27,8 +27,8 @@ let getMapel = async () => {
   let print = "";
   let print1 = "";
   mapel.forEach((item) => {
-    print += `<a class="btn" href="detailMapelSiswa.html">${item.name}</a>`;
-    // detailMapelSiswa.html
+    print += `<button class="btnMapel" id="${item.name}" onclick="hello('${item.name}')">${item.name}</button>`;
+
     print1 += `<div class="col-sm-4">
               <div class="card">
                 <div class="card-body">
@@ -45,25 +45,22 @@ let getMapel = async () => {
 
     dropDownMapel.innerHTML = print;
     cardMapel.innerHTML = print1;
-    console.log(`${item.name}`);
-    localStorage.setItem("username", print.name);
-    // localStorage.setItem("pw", password.value);
   });
 };
 
 getMapel();
 
-// let activeDropDown = document.querySelector(".sidenav .dropdown-container");
-// // var btnContainer = document.getElementById("myDIV");
-// let btnDropDown = activeDropDown.getElementsByClassName("btn");
-// // var btns = btnContainer.getElementsByClassName("btn");
-// // console.log(btnDropDown)
+function hello(pelajaran) {
+  var navPelajaran = document.getElementById(`${pelajaran}`);
+  var pelajaranAct = document.querySelectorAll(`.btnMapel`);
+  localStorage.setItem("pelajaran", pelajaran);
 
-// for (var i = 0; i < btnDropDown.length; i++) {
-//   btnDropDown[i].addEventListener("click", function() {
-//     var current = document.querySelector("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//     console.log('ya')
-//   });
-// }
+  for (i = 0; i <= pelajaranAct.length; i++) {
+    if (!pelajaranAct[i].classList.contains("active")) {
+      navPelajaran.classList.add("active");
+    } else {
+      pelajaranAct[i].classList.remove("active");
+    }
+    console.log(pelajaranAct[i].classList.contains("active"));
+  }
+}
